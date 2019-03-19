e29798bdafbdc15537df24d19e80c32b6b5eba25
SimasDei <simondeiman@gmail.com>
2019-03-19 11:24:14 +0200

    test Nextjs routing and React Components

---
+++ frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Sell = () => {
+  return (
+    <div>
+      <p>Sell Component</p>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Sell;
