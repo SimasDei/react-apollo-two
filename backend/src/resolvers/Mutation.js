a429b4cb2f6c22f2f8aa586304fb759e1a92b6d4
SimasDei <simondeiman@gmail.com>
2019-03-20 10:43:26 +0200

    Join Queries and Mutations. Prisma with Yoga

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutations;
