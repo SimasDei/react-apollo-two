d2869a97815599f03f7e0bdb34bec662e3a97e34
SimasDei <simondeiman@gmail.com>
2019-03-22 17:18:45 +0200

    add image uploading capabilities, use Cloudinary services

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,6 +41,27 @@ export class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    console.log('image upload');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'Dresspress');
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/simas-dei/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -57,6 +78,20 @@ export class CreateItem extends Component {
           >
             <ErrorMessage error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an Image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img src={this.state.image} alt="Upload Preview" />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
